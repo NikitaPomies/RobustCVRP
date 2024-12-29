6850252bf616b22ff69aa12bdefb50750fcc1dc5
bvdka <nikita.pomies@upskills.ai>
2024-12-29 14:36:31 +0100

    add incorrect cuts

--- src/robust_cvrp_cuts.jl
+++ src/robust_cvrp_cuts.jl
@@ -1,64 +1,76 @@
 using JuMP, CPLEX, LinearAlgebra
 
-include("../data/n_10-euclidean_true")
 
+include("instance.jl")
 
-IT = th
-demands = d
-distances = t
-capacity = C
+instance = read_instance("../data/n_9-euclidean_true")
 
+function filter_duplicates(pairs::Vector{Tuple{Int, Int}})
+    unique_pairs = Set{Tuple{Int, Int}}()
+    filtered = Vector{Tuple{Int, Int}}()
 
+    for pair in pairs
+        sorted_pair = (min(pair[1], pair[2]), max(pair[1], pair[2]))
+        if sorted_pair ∉ unique_pairs
+            push!(filtered, pair)
+            push!(unique_pairs, sorted_pair)
+        end
+    end
 
-# Generate a matrix of size n x n with random integers between 0 and 100
-
-# Print the results
-println("Vector IT:")
-println(IT)
+    return filtered
+end
 
 
 function selected_edges(x::Matrix{Float64}, n)
     return Tuple{Int,Int}[(i, j) for i in 1:n, j in 1:n if x[i, j] > 0.5]
 end
 
-function solve_subproblem(x_bar)
-    n = length(demands)
+function solve_subproblem(I::Instance, x_bar)
+    n = length(I.demands)
 
-    selected_e = selected_edges(x_bar, length(demands))
-    sort!(selected_e, by=x -> (IT[x[1]] + IT[x[2]]), rev=true)
+    selected_e = selected_edges(x_bar, n)
+    println(selected_e)
+    selected_e = filter_duplicates(selected_e)
+    println(selected_e)
+
+    sort!(selected_e, by=x_bar -> (I.th[x_bar[1]] + I.th[x_bar[2]]), rev=true)
+    println(selected_e)
+
+
+    
 
     delta_1 = zeros(n, n)
 
     S1 = 0
     for (i, j) in selected_e
-        if S1 + 1 <= T
+        if S1 + 1 <= I.T
             # Can take full item
             delta_1[i, j] = 1.0
             S1 += 1.0
         else
             # Take fractional part
-            remaining = T - S1
+            remaining = I.T - S1
             if remaining > 0
                 delta_1[i, j] = remaining
                 S1 += remaining
             end
-            break  # We've reached T, no need to continue
+            break  # We've reached I.T, no need to continue
         end
     end
 
-    sort!(selected_e, by=x -> (IT[x[1]] * IT[x[2]]), rev=true)
+    sort!(selected_e, by=x_bar -> (I.th[x_bar[1]] * I.th[x_bar[2]]), rev=true)
 
     delta_2 = zeros(n, n)
 
     S2 = 0
     for (i, j) in selected_e
-        if S2 + 2 <= T * T
+        if S2 + 2 <= I.T * I.T
             # Can take full item
             delta_2[i, j] = 2.0
             S2 += 2.0
         else
             # Take fractional part
-            remaining = T * T - S2
+            remaining = I.T * I.T - S2
             if remaining > 0
                 delta_2[i, j] = remaining
                 S2 += remaining
@@ -66,134 +78,202 @@ function solve_subproblem(x_bar)
             break  # We've reached T, no need to continue
         end
     end
-    obj = sum((distances[i, j] + delta_1[i, j] * (IT[i] + IT[j]) + delta_2[i, j] * (IT[i] * IT[j])) * x_bar[i, j] for i in 1:n, j in 1:n if i != j)
+    obj = sum((I.distances[i, j] + delta_1[i, j] * (I.th[i] + I.th[j]) + delta_2[i, j] * (I.th[i] * I.th[j])) * x_bar[i, j] for i in 1:n, j in 1:n if i != j)
     return (obj=obj, delta_1_opt=delta_1,
         delta_2_opt=delta_2)
 
 end
 
+function build_RCVRP_cuts_model(I::Instance)
 
-# Create the model
-model = Model(CPLEX.Optimizer)
-#set_optimizer_attribute(model, "TimeLimit", 5)
-
-
-# Decision variables
-@variable(model, z >= 0)
-@variable(model, x[i=1:n, j=1:n], Bin)  # Binary variable: 1 if arc (i, j) is used
-@variable(model, u[1:n] >= 0)           # MTZ variables: auxiliary variables for subtour elimination
+    # Create the model
+    model = Model(CPLEX.Optimizer)
+    #set_optimizer_attribute(model, "TimeLimit", 5)
 
 
+    # Decision variables
+    @variable(model, z >= 0)
+    @variable(model, x[i=1:n, j=1:n], Bin)  # Binary variable: 1 if arc (i, j) is used
+    @variable(model, u[1:n] >= 0)           # MTZ variables: auxiliary variables for subtour elimination
 
-@constraint(model, [j in 2:n], sum(x[:, j]) == 1)  # Each node is visited once
 
-@constraint(model, [i in 2:n], sum(x[i, :]) == 1)  # Each node is visited once
 
-# MTZ subtour elimination constraints
-for i in 2:n, j in 2:n
-    if i != j
-        @constraint(model, u[i] - u[j] <= (capacity) * (1 - x[i, j]) - demands[j])
+    @constraint(model, [j in 2:n], sum(x[:, j]) == 1)  # Each node is visited once
+
+    @constraint(model, [i in 2:n], sum(x[i, :]) == 1)  # Each node is visited once
+
+    # MTZ subtour elimination constraints
+    for i in 2:n, j in 2:n
+        if i != j
+            @constraint(model, u[i] - u[j] <= (I.capacity) * (1 - x[i, j]) - I.demands[j])
+        end
     end
-end
 
-for i in 2:n, j in 2:n
-    if i != j
-        @constraint(model, x[i, j] + x[j, i] <= 1)
+    for i in 2:n, j in 2:n
+        if i != j
+            @constraint(model, x[i, j] + x[j, i] <= 1)
+        end
     end
+
+    @constraint(model, [i in 2:n], I.demands[i] <= u[i] <= I.capacity)
+
+
+    @constraint(model, sum(x[1, j] for j in 2:n) == sum(x[j, 1] for j in 2:n))
+
+
+
+
+
+    # Constraints
+    #@constraint(model, sum(x[1, j] for j in 2:n) == 2)  # 8 vehicles leave the depot
+    #@constraint(model, sum(x[i, 1] for i in 2:n) == 2)  # 8 vehicles return to the depot
+
+    #z constraint 
+    @constraint(model, obj_cstr, sum(I.distances[i, j] * x[i, j] for i in 1:n, j in 1:n if i != j) <= z)
+
+    for i in 1:n
+        @constraint(model, x[i, i] == 0)
+    end
+
+    @objective(model, Min, z)
+
+
+    return model
 end
 
-@constraint(model, [i in 2:n], demands[i] <= u[i] <= capacity)
+model = build_RCVRP_cuts_model(instance)
 
+function solve_RCVRP_iterative_cuts(I::Instance, model)
 
-@constraint(model, sum(x[1, j] for j in 2:n) == sum(x[j, 1] for j in 2:n))
-
-
-
-
-
-# Constraints
-#@constraint(model, sum(x[1, j] for j in 2:n) == 2)  # 8 vehicles leave the depot
-#@constraint(model, sum(x[i, 1] for i in 2:n) == 2)  # 8 vehicles return to the depot
-
-#z constraint 
-@constraint(model, obj_cstr, sum(distances[i, j] * x[i, j] for i in 1:n, j in 1:n if i != j) <= z)
-
-
-@objective(model, Min, z)
-
-for i in 1:n
-    @constraint(model, x[i, i] == 0)
-end
-
-MAXIMUM_ITERATIONS = 100
-#set_silent(model)
-
-optimize!(model)
-
-@assert is_solved_and_feasible(model)
-
-for k in 1:MAXIMUM_ITERATIONS
-    println("Iteration $(k)")
+    MAXIMUM_ITERATIONS = 100
     #set_silent(model)
 
+    optimize!(model)
+    println(objective_value(model))
 
     @assert is_solved_and_feasible(model)
-    lower_bound = objective_value(model)
-    x_k = value.(x)
-    ret = solve_subproblem(x_k)
 
-    println(lower_bound - ret.obj)
-    for i in 1:n
-        for j in 1:n
-            if ret.delta_1_opt[i, j] > 0
-                println(x_k[i, j])
+    for k in 1:MAXIMUM_ITERATIONS
+        println("Iteration $(k)")
+        #set_silent(model)
+
+
+        @assert is_solved_and_feasible(model)
+        lower_bound = objective_value(model)
+        x_k = value.(model[:x])
+        ret = solve_subproblem(I, x_k)
+
+        println(lower_bound - ret.obj)
+        for i in 1:n
+            for j in 1:n
+                if ret.delta_1_opt[i, j] > 0
+                    println(x_k[i, j])
+                end
             end
         end
-    end
-    
-    if lower_bound >= ret.obj
-        break
-    end
+
+        if lower_bound >= ret.obj
+            break
+        end
 
 
-    new_distances = zeros(n, n)
-    for i in 1:n
-        for j in 1:n
-            new_distances[i, j] = distances[i, j] + ret.delta_1_opt[i, j] * (IT[i] + IT[j]) + ret.delta_2_opt[i, j] * (IT[i] * IT[j])
-            if (new_distances[j, i] < new_distances[i, j])
-                new_distances[j, i] = new_distances[i, j] # symmetric 
-            end
-            if (new_distances[j, i] > new_distances[i, j])
-                new_distances[i, j] = new_distances[j, i] # symmetric 
-            end
-            if ((x_k[i, j] > 0) && (ret.delta_1_opt[i, j] > 0 || ret.delta_2_opt[i, j] > 0))
-                println("Modifying distance ", i, j)
+        new_distances = zeros(n, n)
+        for i in 1:n
+            for j in 1:n
+                new_distances[i, j] = I.distances[i, j] + ret.delta_1_opt[i, j] * (I.th[i] + I.th[j]) + ret.delta_2_opt[i, j] * (I.th[i] * I.th[j])
+                if (new_distances[j, i] < new_distances[i, j])
+                    new_distances[j, i] = new_distances[i, j] # symmetric 
+                end
+                if (new_distances[j, i] > new_distances[i, j])
+                    new_distances[i, j] = new_distances[j, i] # symmetric 
+                end
+                if ((x_k[i, j] > 0) && (ret.delta_1_opt[i, j] > 0 || ret.delta_2_opt[i, j] > 0))
+                    println("Modifying distance ", i, j)
+                end
             end
         end
+
+        z = value.(model[:z])
+        println("sum before : ", z - sum(new_distances[i, j] * x_k[i, j] for i in 1:n, j in 1:n if i != j))
+        cut = @constraint(model, sum(new_distances[i, j] * model[:x][i, j] for i in 1:n, j in 1:n if i != j) <= model[:z])
+
+        set_silent(model)
+        optimize!(model)
+        x_k = value.(model[:x])
+        z = value.(model[:z])
+
+
+        println("sum after : ", z - sum(new_distances[i, j] * x_k[i, j] for i in 1:n, j in 1:n if i != j))
+        println("VRP value :", z)
+
+    end
+    return model
+end
+
+function solve_RCVRP_callback_cuts(I::Instance, model)
+
+    function cut_callback(cb_data)
+        status = callback_node_status(cb_data, model)
+        if status != MOI.CALLBACK_NODE_STATUS_INTEGER
+            return
+        end
+
+        x_k = callback_value.(cb_data, model[:x])
+        lower_bound =  callback_value.(cb_data, model[:z])
+
+
+
+        ret = solve_subproblem(I, x_k)
+        if lower_bound >= ret.obj
+            return
+        end
+
+
+
+        new_distances = zeros(n, n)
+        for i in 1:n
+            for j in 1:n
+                new_distances[i, j] = I.distances[i, j] + ret.delta_1_opt[i, j] * (I.th[i] + I.th[j]) + ret.delta_2_opt[i, j] * (I.th[i] * I.th[j])
+                if (new_distances[j, i] < new_distances[i, j])
+                    new_distances[j, i] = new_distances[i, j] # symmetric 
+                end
+                if (new_distances[j, i] > new_distances[i, j])
+                    new_distances[i, j] = new_distances[j, i] # symmetric 
+                end
+              #=   if ((x_k[i, j] > 0) && (ret.delta_1_opt[i, j] > 0 || ret.delta_2_opt[i, j] > 0))
+                    println("Modifying distance ", i, j)
+                end =#
+            end
+        end
+
+        cut = @build_constraint(sum(new_distances[i, j] * model[:x][i, j] for i in 1:n, j in 1:n if i != j) <= model[:z])
+        MOI.submit(model, MOI.LazyConstraint(cb_data), cut)
     end
 
-
-    println("sum before : ", value.(z) - sum(new_distances[i, j] * x_k[i, j] for i in 1:n, j in 1:n if i != j))
-    cut = @constraint(model, sum(new_distances[i, j] * x[i, j] for i in 1:n, j in 1:n if i != j) <= z)
-
-    set_silent(model)
+    set_attribute(
+        model,
+        MOI.LazyConstraintCallback(),
+        cut_callback,
+    )
     optimize!(model)
-    x_k = value.(x)
+    return model
 
-    println("sum after : ", value.(z) - sum(new_distances[i, j] * x_k[i, j] for i in 1:n, j in 1:n if i != j))
-    println("VRP value :", value.(z))
 
 end
+
 
 
 
 
 # Solve the model
+solved_model = solve_RCVRP_iterative_cuts(instance, model)
+#solved_model = solve_RCVRP_callback_cuts(instance, model)
 
 # Extract and print the solution
-if termination_status(model) == MOI.OPTIMAL
-    println("Optimal objective value: ", objective_value(model))
-    solution = [(i, j) for i in 1:n, j in 1:n if value(x[i, j]) > 0.5]
+if termination_status(solved_model) == MOI.OPTIMAL
+    println("Optimal objective value: ", objective_value(solved_model))
+    x_sol = value.(solved_model[:x])
+    solution = [(i, j) for i in 1:n, j in 1:n if value(x_sol[i, j]) > 0.5]
     println(solution)
 
 else
--- src/robust_cvrp_dualisation.jl
+++ src/robust_cvrp_dualisation.jl
@@ -3,7 +3,7 @@ using JuMP, CPLEX, LinearAlgebra
 
 include("instance.jl")
 
-instance  = read_instance("../data/n_20-euclidean_true")
+instance  = read_instance("../data/n_9-euclidean_true")
 
 
 function build_RCVRP_dual_model(I::Instance)
