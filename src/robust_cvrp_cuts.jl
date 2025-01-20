347bdd41d07e474449e283773cdd2ba64825176f
Rayan <hamoud.rayan@hotmail.fr>
2025-01-20 16:33:26 +0100

    continuous knapsack cuts

--- src/robust_cvrp_cuts.jl
+++ src/robust_cvrp_cuts.jl
@@ -3,7 +3,7 @@ using JuMP, CPLEX, LinearAlgebra
 
 include("instance.jl")
 
-instance = read_instance("../data/n_9-euclidean_true")
+instance = read_instance("../data/n_10-euclidean_true")
 
 function find_one_route_clients(pairs::Vector{Tuple{Int,Int}}, n::Int64)
     point_to_depot = Dict()
@@ -24,62 +24,73 @@ function selected_edges(x::Matrix{Float64}, n)
     return Tuple{Int,Int}[(i, j) for i in 1:n, j in 1:n if x[i, j] > 0.5]
 end
 
+function fractional_knapsack(weights::Dict{Tuple{Int, Int}, Float64}, 
+                             values::Dict{Tuple{Int, Int}, Float64}, 
+                             capacity::Float64, max_var::Float64 = 1.0)
+    """
+    Résoud le problème du sac à dos continu avec max_var la borne supérieure de la variable de choix continue
+    """
+    # Calcul des ratios valeur/poids pour chaque (i, j)
+    items = [(values[k] / weights[k], k, weights[k], values[k]) for k in keys(weights)]
+    
+    # Tri par ratio décroissant
+    sort!(items, by = x -> x[1], rev = true)
+    
+    total_value = 0.0
+    remaining_capacity = capacity
+    solution = Dict((i, j) => 0.0 for (i,j) in keys(weights))  
+
+    for (_, (i, j), weight, value) in items
+        if remaining_capacity == 0
+            break
+        end
+
+        if weight*max_var <= remaining_capacity
+            total_value += value*max_var
+            remaining_capacity -= weight*max_var
+            solution[(i, j)] = max_var  
+        else
+            fraction = remaining_capacity/weight
+            total_value += value * fraction
+            solution[(i, j)] = fraction 
+            remaining_capacity = 0
+        end
+    end
+
+    return total_value, solution
+end
+
+
 function solve_subproblem(I::Instance, x_bar)
     n = length(I.demands)
 
     selected_e = selected_edges(x_bar, n)
-    #println(selected_e)
-    # selected_e = filter_duplicates(selected_e)
-    #println(selected_e)
-
-    sort!(selected_e, by=x -> (I.th[x[1]] + I.th[x[2]]), rev=true)
-    #println(selected_e)
-
-
-
-
-    delta_1 = zeros(n, n)
-
-    S1 = 0
-    for (i, j) in selected_e
-        if S1 + 1 <= I.T
-            # Can take full item
-            delta_1[i, j] = 1.0
-            S1 += 1.0
-        else
-            # Take fractional part
-            remaining = I.T - S1
-            if remaining > 0
-                delta_1[i, j] = remaining
-                S1 += remaining
-            end
-            break  # We've reached I.T, no need to continue
-        end
+    
+    values_1 = Dict((i, j) => 0. for i in 1:n, j in 1:n)
+    for (i,j) in selected_e
+        values_1[i,j] = Float64(I.th[i]+I.th[j]) 
     end
+    
+    weights_1 = Dict((i, j) => 1.0 for i in 1:n, j in 1:n)
+    capacity_1 = Float64(I.T)
+    
+    val_1, delta_1 = fractional_knapsack(weights_1, values_1, capacity_1)
 
-    sort!(selected_e, by=x -> (I.th[x[1]] * I.th[x[2]]), rev=true)
-
-    delta_2 = zeros(n, n)
-
-    S2 = 0
-    for (i, j) in selected_e
-        if S2 + 2 <= I.T * I.T
-            # Can take full item
-            delta_2[i, j] = 2.0
-            S2 += 2.0
-        else
-            # Take fractional part
-            remaining = I.T * I.T - S2
-            if remaining > 0
-                delta_2[i, j] = remaining
-                S2 += remaining
-            end
-            break  # We've reached T, no need to continue
-        end
+    values_2 = Dict((i, j) => 0. for i in 1:n, j in 1:n)
+    for (i,j) in selected_e
+        values_2[i,j] = Float64(I.th[i]*I.th[j]) 
     end
-    obj = sum((I.distances[i, j] + delta_1[i, j] * (I.th[i] + I.th[j]) + delta_2[i, j] * (I.th[i] * I.th[j])) * x_bar[i, j] for i in 1:n, j in 1:n if i != j)
-    return (obj=obj, delta_1_opt=delta_1,
-        delta_2_opt=delta_2)
+    
+    weights_2 = Dict((i, j) => 1.0 for i in 1:n, j in 1:n)
+    capacity_2 = Float64(I.T*I.T)
+    
+    val_2, delta_2 = fractional_knapsack(weights_2, values_2, capacity_2, 2.0)
+    
+    #obj = sum((I.distances[i, j] + delta_1[i, j] * (I.th[i] + I.th[j]) + delta_2[i, j] * (I.th[i] * I.th[j])) * x_bar[i, j] for i in 1:n, j in 1:n if i != j)
+    
+    obj = sum((I.distances[i, j]) * x_bar[i, j] for i in 1:n, j in 1:n if i != j)+val_1+val_2
+
+    return (obj=obj, delta_1_opt=delta_1, delta_2_opt=delta_2)
 
 end
 
@@ -95,10 +106,7 @@ function build_RCVRP_cuts_model(I::Instance)
     @variable(model, x[i=1:n, j=1:n], Bin)  # Binary variable: 1 if arc (i, j) is used
     @variable(model, u[1:n] >= 0)           # MTZ variables: auxiliary variables for subtour elimination
 
-
-
     @constraint(model, [j in 2:n], sum(x[:, j]) == 1)  # Each node is visited once
-
     @constraint(model, [i in 2:n], sum(x[i, :]) == 1)  # Each node is visited once
 
     # MTZ subtour elimination constraints
@@ -116,12 +124,7 @@ function build_RCVRP_cuts_model(I::Instance)
 
     @constraint(model, [i in 2:n], I.demands[i] <= u[i] <= I.capacity)
 
-
     @constraint(model, sum(x[1, j] for j in 2:n) == sum(x[j, 1] for j in 2:n))
-
-
-
-
 
     # Constraints
     #@constraint(model, sum(x[1, j] for j in 2:n) == 2)  # 8 vehicles leave the depot
@@ -136,7 +139,6 @@ function build_RCVRP_cuts_model(I::Instance)
     end
 
     @objective(model, Min, z)
-
 
     return model
 end
@@ -272,12 +274,7 @@ function solve_RCVRP_callback_cuts(I::Instance, model)
     optimize!(model)
     return model
 
-
 end
-
-
-
-
 
 # Solve the model
 solved_model = solve_RCVRP_iterative_cuts(instance, model)
@@ -293,4 +290,5 @@ if termination_status(solved_model) == MOI.OPTIMAL
 else
     println("No optimal solution found.")
 end
+
 
